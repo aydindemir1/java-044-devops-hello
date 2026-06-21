0e0380bede05692f344e8846a93c812f0540c39c
aydin <aydin@AYDIN>
2026-06-21 20:21:32 +0300

    Initial commit - Spring Boot project

---
+++ src/main/java/com/aydindemir/Java044DevopsHelloApplication.java
@@ -0,0 +1,13 @@
+package com.aydindemir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java044DevopsHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java044DevopsHelloApplication.class, args);
+	}
+
+}
